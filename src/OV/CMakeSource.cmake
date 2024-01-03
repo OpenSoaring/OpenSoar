0cc578c00abe07fa91accb31fc6eab8525b44d4f
August2111 <Info@FlapsOnline.de>
2024-01-03 10:33:30 +0100

    [CMake/OV] - add FileMenuWidget

--- src/OV/CMakeSource.cmake
+++ src/OV/CMakeSource.cmake
@@ -2,8 +2,10 @@ set(_SOURCES
         OV/OpenVarioMenu.cpp
         OV/System.cpp
         OV/Settings.cpp
+        OV/FileMenuWidget.cpp
 )
 
 set(SCRIPT_FILES
     CMakeSource.cmake
+    ../../build/ov.mk
 )
