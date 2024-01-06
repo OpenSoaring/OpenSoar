0ab9bb9676a8d3a4925289d43f2c5cc8b9607e0d
August2111 <Info@FlapsOnline.de>
2024-01-06 11:22:04 +0100

    [CMake] - OpenVarioMenu - start reorganize

---
+++ src/OpenVario/CMakeSource.cmake
@@ -0,0 +1,12 @@
+set(_SOURCES
+        OpenVario/OpenVarioMenu.cpp
+        OpenVario/System/System.cpp
+        OpenVario/System/SystemMenuWidget.cpp
+        OpenVario/System/SystemSettingsWidget.cpp
+        OpenVario/FileMenuWidget.cpp
+)
+
+set(SCRIPT_FILES
+    CMakeSource.cmake
+    ../../build/ov.mk
+)
