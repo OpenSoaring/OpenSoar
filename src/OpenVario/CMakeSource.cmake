a0d0191b4866aa150f02a6d698c726718db29271
August2111 <Info@FlapsOnline.de>
2024-01-06 11:23:43 +0100

    [CMake] OpenVarioMenu - split the (Widget) classes in different source files

--- src/OpenVario/CMakeSource.cmake
+++ src/OpenVario/CMakeSource.cmake
@@ -1,20 +1,32 @@
 set(_SOURCES
         OpenVario/OpenVarioMenu.cpp
+
+        OpenVario/FileMenuWidget.cpp
+
         OpenVario/System/System.cpp
         OpenVario/System/SystemMenuWidget.cpp
         OpenVario/System/SystemSettingsWidget.cpp
-        OpenVario/FileMenuWidget.cpp
 
         ${SRC}/Version.cpp
        	${SRC}/Asset.cpp
+        ${SRC}/Dialogs/DialogSettings.cpp
+        ${SRC}/Dialogs/WidgetDialog.cpp
+        ${SRC}/Dialogs/HelpDialog.cpp
+        ${SRC}/Dialogs/Message.cpp
+        ${SRC}/Dialogs/LockScreen.cpp
+        ${SRC}/Dialogs/TextEntry.cpp
+        ${SRC}/Dialogs/KnobTextEntry.cpp
+        ${SRC}/Dialogs/TouchTextEntry.cpp
+        ${SRC}/Form/DigitEntry.cpp
         ${SRC}/Formatter/HexColor.cpp
         ${SRC}/Formatter/TimeFormatter.cpp
+        ${SRC}/Gauge/LogoView.cpp
         ${SRC}/Hardware/CPU.cpp
         ${SRC}/Hardware/DisplayDPI.cpp
         ${SRC}/Hardware/RotateDisplay.cpp
         ${SRC}/Hardware/DisplayGlue.cpp
-        ${SRC}/Screen/Layout.cpp
-        ${SRC}/ui/control/TerminalWindow.cpp
+        ${SRC}/LogFile.cpp
+        ${SRC}/LocalPath.cpp
         ${SRC}/Look/TerminalLook.cpp
         ${SRC}/Look/DialogLook.cpp
         ${SRC}/Look/ButtonLook.cpp
@@ -33,6 +45,27 @@ set(_SOURCES
         ${SRC}/Profile/File.cpp
         ${SRC}/Profile/NumericValue.cpp
 
+        ${SRC}/ProgressGlue.cpp
+        ${SRC}/ProgressWindow.cpp
+
+        ${SRC}/Renderer/TwoTextRowsRenderer.cpp
+        ${SRC}/Screen/Layout.cpp
+
+        ${SRC}/io/FileOutputStream.cxx
+        
+        ${SRC}/lib/fmt/SystemError.cxx
+        ${SRC}/Units/Descriptor.cpp
+
+        ${SRC}/ResourceLoader.cpp
+        ${SRC}/ui/control/TerminalWindow.cpp
+        ${SRC}/ui/canvas/gdi/Canvas.cpp
+        ${SRC}/ui/canvas/gdi/Bitmap.cpp
+        ${SRC}/ui/canvas/gdi/GdiPlusBitmap.cpp
+        ${SRC}/ui/canvas/gdi/ResourceBitmap.cpp
+
+        ${SRC}/Interface.cpp
+        ${SRC}/Blackboard/InterfaceBlackboard.cpp
+        ${SRC}/MainWindow.cpp
 )
 
 set(SCRIPT_FILES
