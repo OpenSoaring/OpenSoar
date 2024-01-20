48e929368c127225093b9c465845f1284e25daf9
August2111 <Info@FlapsOnline.de>
2024-01-20 20:18:28 +0100

    SystemMenuWidget.cpp - use GetHomePath
    use Run with output file
    use define switch to select DBUS or cmd line for ...
     setting VarioD and SensorD

--- src/OpenVario/System/SystemMenuWidget.cpp
+++ src/OpenVario/System/SystemMenuWidget.cpp
@@ -22,10 +22,13 @@
 #include "ui/window/Init.hpp"
 #include "util/ScopeExit.hxx"
 
+#include "OpenVario/SystemSettingsWidget.hpp"
 #include "OpenVario/System/OpenVarioDevice.hpp"
-#include "OpenVario/System/SystemSettingsWidget.hpp"
 #include "OpenVario/System/SystemMenuWidget.hpp"
 
+#include "system/Process.hpp"
+
+#include <iostream>
 #include <string>
 #include <fmt/format.h>
 
@@ -94,6 +97,84 @@ CalibrateSensors() noexcept
 }
 
 //-----------------------------------------------------------------------------
+class ScreenSSHWidget final : public RowFormWidget {
+  UI::Display &display;
+  UI::EventQueue &event_queue;
+
+public:
+  ScreenSSHWidget(UI::Display &_display, UI::EventQueue &_event_queue,
+                  const DialogLook &look) noexcept
+      : RowFormWidget(look), display(_display), event_queue(_event_queue) {}
+
+private:
+  /* virtual methods from class Widget */
+  void Prepare(ContainerWindow &parent, const PixelRect &rc) noexcept override;
+};
+
+void ScreenSSHWidget::Prepare([[maybe_unused]] ContainerWindow &parent,
+                              [[maybe_unused]] const PixelRect &rc) noexcept {
+  AddButton(_T("Enable"), []() {
+    static constexpr const char *argv[] = {
+        "/bin/sh", "-c",
+        "systemctl enable --now dropbear.socket && printf '\nSSH has been enabled'",
+
+        nullptr};
+
+    RunProcessDialog(UIGlobals::GetMainWindow(), UIGlobals::GetDialogLook(),
+                     _T("Enable"), argv);
+  });
+
+  AddButton(_T("Disable"), []() {
+    static constexpr const char *argv[] = {
+        "/bin/sh", "-c",
+        "systemctl disable --now dropbear.socket && printf '\nSSH has been disabled'",
+        nullptr};
+
+    RunProcessDialog(UIGlobals::GetMainWindow(), UIGlobals::GetDialogLook(),
+                     _T("Disable"), argv);
+  });
+
+  AddButton(_T("IsEnabled"), []() {
+    static constexpr const char *argv[] = {
+        "/bin/sh", "-c",
+        "systemctl is-enabled dropbear.socket",
+        nullptr};
+
+    RunProcessDialog(UIGlobals::GetMainWindow(), UIGlobals::GetDialogLook(),
+                     _T("IsEnabled"), argv);
+  });
+  AddButton(_T("IsActive"), []() {
+    static constexpr const char *argv[] = {
+        "/bin/sh", "-c",
+        "systemctl is-active dropbear.socket",
+        nullptr};
+
+    RunProcessDialog(UIGlobals::GetMainWindow(), UIGlobals::GetDialogLook(),
+                     _T("IsActive"), argv);
+  });
+
+  AddButton(_T("GetStatus"), []() {
+    static constexpr const char *argv[] = {
+        "/bin/sh", "-c",
+        "systemctl is-enabled dropbear.socket", 
+        "systemctl is-active dropbear.socket", nullptr};
+
+    RunProcessDialog(UIGlobals::GetMainWindow(), UIGlobals::GetDialogLook(),
+                     _T("GetStatus"), argv);
+  });
+  AddButton(_T("GetStatus2"), []() {
+    static constexpr const char *argv[] = {
+        "/bin/sh", "-c",
+        "/bin/systemctl is-enabled dropbear.socket", 
+        "echo '\n ===== \n'", 
+        "/bin/systemctl is-active dropbear.socket", nullptr};
+
+    RunProcessDialog(UIGlobals::GetMainWindow(), UIGlobals::GetDialogLook(),
+                     _T("GetStatus2"), argv);
+  });
+}
+
+//-----------------------------------------------------------------------------
 void
 SystemMenuWidget::Prepare([[maybe_unused]] ContainerWindow &parent,
                           [[maybe_unused]] const PixelRect &rc) noexcept
@@ -101,6 +182,15 @@ SystemMenuWidget::Prepare([[maybe_unused]] ContainerWindow &parent,
   AddButton(_("Upgrade Firmware"), [this]() {
     // dialog.SetModalResult(START_UPGRADE);
     exit(START_UPGRADE);
+  });
+
+  AddButton(_T("SSH"), [this]() {
+    TWidgetDialog<ScreenSSHWidget> sub_dialog(WidgetDialog::Full{},
+                                              dialog.GetMainWindow(), GetLook(),
+                                              _T("Enable or Disable SSH"));
+    sub_dialog.SetWidget(display, event_queue, GetLook());
+    sub_dialog.AddButton(_("Close"), mrOK);
+    return sub_dialog.ShowModal();
   });
 
   AddButton(_("WiFi Settings"), []() {
@@ -147,5 +237,32 @@ SystemMenuWidget::Prepare([[maybe_unused]] ContainerWindow &parent,
                      UIGlobals::GetDialogLook(),
                      _T("System Info"), argv);
   });
+  AddButton(_("Test-Process"), []() {
+    static constexpr const char *argv[] = {
+      "/bin/ls", nullptr
+    };
+
+    RunProcessDialog(UIGlobals::GetMainWindow(),
+                     UIGlobals::GetDialogLook(),
+                     _T("Test-Process"), argv);
+  });
+
+  AddButton(_("Test-Process 2"), [this]() {
+    StaticString<0x200> str;
+    str.Format(_T("%s/%s"), ovdevice.GetHomePath().c_str(), _T("process.txt"));
+    Path output = Path(str);
+    std::cout << "FileName: " << output.ToUTF8() << std::endl;
+
+    auto ret_value = Run(
+      output,
+      "/home/august2111/TestProcess.sh");
+
+    char buffer[0x100];
+    File::ReadString(output, buffer, sizeof(buffer));
+    std::string xx = buffer; 
+    xx += "\nreturn value: " + std::to_string(ret_value);  
+    File::WriteExisting(output, xx.c_str());
+    return ret_value;
+    });
 }
 
