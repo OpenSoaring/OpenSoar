17fd60bb7c0eac621c49d0213d8206c49af390b8
August2111 <Info@FlapsOnline.de>
2024-03-27 18:14:27 +0100

    [OV] OpenVario - rework menu structure

--- src/OpenVario/System/Setting/RotationWidget.cpp
+++ src/OpenVario/System/Setting/RotationWidget.cpp
@@ -61,41 +61,45 @@ private:
   void SaveRotation(const std::string &rotationvalue);
 };
 
+/*/
 void
-SettingRotationWidget::SaveRotation(const std::string &rotationString)
+SettingRotationWidget::SaveRotation(const int rotationInt)
 {
+
    File::WriteExisting(Path(_T("/sys/class/graphics/fbcon/rotate")), (rotationString).c_str());
-   int rotationInt = stoi(rotationString);
+//   int rotationInt = stoi(rotationString);
    // ChangeConfigInt("rotation", rotationInt, ovdevice.GetSettingsConfig());
    // TODO(August2111):  move the from ovdevice.settings to ovdevice.sysetm
    LoadConfigFile(ovdevice.system_map, ovdevice.GetSystemConfig());  // -> OpenVarioBaseMenu->StartUp!
    ovdevice.rotation = (DisplayOrientation) stoi(rotationString);
-   ovdevice.system_map.insert_or_assign("Rotation", std::to_string((unsigned) ovdevice.rotation));
+   ovdevice.system_map.insert_or_assign("rotation", std::to_string((unsigned) ovdevice.rotation));
    WriteConfigFile(ovdevice.system_map, ovdevice.GetSystemConfig());
 }
+*/
 
 void
 SettingRotationWidget::Prepare([[maybe_unused]] ContainerWindow &parent,
                              [[maybe_unused]] const PixelRect &rc) noexcept
 {
  AddButton(_T("Landscape"), [this](){
-	SaveRotation("0");
-   Display::Rotate(DisplayOrientation::LANDSCAPE);
+	  // SaveRotation("2");
+   // Display::Rotate(DisplayOrientation::LANDSCAPE);
+    ovdevice.SetRotation(DisplayOrientation::LANDSCAPE);
  });
 
  AddButton(_T("Portrait (90°)"), [this](){
-   SaveRotation("1");
-   Display::Rotate(DisplayOrientation::REVERSE_PORTRAIT);
+   //SaveRotation("1");
+   ovdevice.SetRotation(DisplayOrientation::REVERSE_PORTRAIT);
  });
 
  AddButton(_T("Landscape (180°)"), [this](){
-   SaveRotation("2");
-   Display::Rotate(DisplayOrientation::REVERSE_LANDSCAPE);
+   // SaveRotation("4");
+   ovdevice.SetRotation(DisplayOrientation::REVERSE_LANDSCAPE);
  });
 
  AddButton(_T("Portrait (270°)"), [this](){
-   SaveRotation("3");
-   Display::Rotate(DisplayOrientation::PORTRAIT);
+   // SaveRotation("3");
+   ovdevice.SetRotation(DisplayOrientation::PORTRAIT);
  });
 }
 
@@ -109,9 +113,3 @@ ShowRotationSettingsWidget(ContainerWindow &parent,
  sub_dialog.AddButton(_("Close"), mrOK);
  return sub_dialog.ShowModal();
 }
-
-// std::unique_ptr<Widget>
-// CreateSettingRotationWidget() noexcept
-// {
-//   return std::make_unique<SettingRotationWidget>();
-// }
