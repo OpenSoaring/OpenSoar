3bec94ae8f0ca70ec34ffed82ef08218c458af15
August2111 <Info@FlapsOnline.de>
2024-04-13 17:24:02 +0200

    Dialogs/TextEntry.cpp - use TouchTextEntry as default (before KnobTextEntry)

--- src/Dialogs/TextEntry.cpp
+++ src/Dialogs/TextEntry.cpp
@@ -13,14 +13,18 @@ TextEntryDialog(TCHAR *text, size_t width,
 {
   switch (UIGlobals::GetDialogSettings().text_input_style) {
   case DialogSettings::TextInputStyle::Default:
+    return TouchTextEntry(text, width, caption, accb, default_shift_state);
   case DialogSettings::TextInputStyle::Keyboard:
+#if 1
+    return TouchTextEntry(text, width, caption, accb, default_shift_state);
+#else
     if (HasPointer())
       return TouchTextEntry(text, width, caption, accb, default_shift_state);
     else {
       KnobTextEntry(text, width, caption);
       return true;
     }
-
+#endif
   case DialogSettings::TextInputStyle::HighScore:
     KnobTextEntry(text, width, caption);
     return true;
