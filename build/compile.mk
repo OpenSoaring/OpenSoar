ab43a7ab185a3b663dcba4a8515de8ab29e6783a
August2111 <Info@FlapsOnline.de>
2024-10-09 14:04:04 +0200

    [build] build/compile.mk change CPP to CXX

--- build/compile.mk
+++ build/compile.mk
@@ -117,7 +117,7 @@ $(ABI_OUTPUT_DIR)/%$(OBJ_SUFFIX): %.c | $(ABI_OUTPUT_DIR)/%/../dirstamp $(compil
 # cxx-flags += -v
 
 $(ABI_OUTPUT_DIR)/%$(OBJ_SUFFIX): %.cpp | $(ABI_OUTPUT_DIR)/%/../dirstamp $(compile-depends)
-	@$(NQ)echo "  CPP     $@"
+	@$(NQ)echo "  CXX     $@"
 	$(Q)$(WRAPPED_CXX) $< -c -o $@ $(cxx-flags)
 ifeq ($(IWYU),y)
 	$(Q)iwyu $< $(cxx-flags)
