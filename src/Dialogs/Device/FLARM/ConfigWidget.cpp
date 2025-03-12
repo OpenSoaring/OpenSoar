ca30a2544575d982dc78eedd6c20686afbab9df4
Stefan Schumann <stefan.schumann@posteo.de>
2025-03-12 12:52:51 +0100

    Dialogs/Device/FLARM/ConfigWidget: move Stealth mode next to No track
    Easier to follow the dialog that way. All code references to priv/no track have been moved, even if they do not update the display so reading the code is easier.
    
    # Conflicts:
    #	src/Dialogs/Device/FLARM/ConfigWidget.cpp

--- src/Dialogs/Device/FLARM/ConfigWidget.cpp
+++ src/Dialogs/Device/FLARM/ConfigWidget.cpp
@@ -15,11 +15,11 @@ FlarmHardware hardware;
 
 static const char *const flarm_setting_names[] = {
   "BAUD",
-  "PRIV",
   "THRE",
   "RANGE",
   "ACFT",
   "LOGINT",
+  "PRIV",
   "NOTRACK",
   NULL
 };
@@ -49,12 +49,12 @@ FLARMConfigWidget::Prepare([[maybe_unused]] ContainerWindow &parent,
     device.RequestAllSettings(adsb_setting_names, env);
 
   baud = device.GetUnsignedValue("BAUD", 2);
-  priv = device.GetUnsignedValue("PRIV", 0) == 1;
   thre = device.GetUnsignedValue("THRE", 2);
   unsigned max_range = hardware.isPowerFlarm() ? 65535 : 25500;
   range = device.GetUnsignedValue("RANGE", max_range);
   acft = device.GetUnsignedValue("ACFT", 0);
   log_int = device.GetUnsignedValue("LOGINT", 2);
+  priv = device.GetUnsignedValue("PRIV", 0) == 1;
   notrack = device.GetUnsignedValue("NOTRACK", 0) == 1;
 
   static constexpr StaticEnumChoice baud_list[] = {
@@ -67,7 +67,7 @@ FLARMConfigWidget::Prepare([[maybe_unused]] ContainerWindow &parent,
   };
 
   AddEnum(_("Baud rate"), NULL, baud_list, baud);
-  AddBoolean(_("Stealth mode"), NULL, priv);
+  // Disabled Scumi? AddBoolean(_("Stealth mode"), NULL, priv);
   AddInteger(_("Threshold"), NULL, "%d m/s", "%d", 1, 10, 1, thre);
   AddInteger(_("Range"), NULL, "%d m", "%d", 2000, max_range, 250, range);
 
@@ -108,6 +108,7 @@ FLARMConfigWidget::Prepare([[maybe_unused]] ContainerWindow &parent,
   AddEnum(_("Type"), NULL, acft_list, acft);
   AddInteger(_("Logger interval"), NULL, "%d s", "%d",
              1, 8, 1, log_int);
+  AddBoolean(_("Stealth mode"), NULL, priv);
   AddBoolean(_("No tracking mode"), NULL, notrack);
 
 }
@@ -122,12 +123,6 @@ try {
   if (SaveValueEnum(Baud, baud)) {
     buffer.UnsafeFormat("%u", baud);
     device.SendSetting("BAUD", buffer, env);
-    changed = true;
-  }
-
-  if (SaveValue(Priv, priv)) {
-    buffer.UnsafeFormat("%u", priv);
-    device.SendSetting("PRIV", buffer, env);
     changed = true;
   }
 
@@ -186,6 +181,12 @@ try {
   if (SaveValueInteger(LogInt, log_int)) {
     buffer.UnsafeFormat("%u", log_int);
     device.SendSetting("LOGINT", buffer, env);
+    changed = true;
+  }
+
+  if (SaveValue(Priv, priv)) {
+    buffer.UnsafeFormat("%u", priv);
+    device.SendSetting("PRIV", buffer, env);
     changed = true;
   }
 
