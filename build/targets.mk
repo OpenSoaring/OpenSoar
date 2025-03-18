4c9e730f54948b56f8433b21c609a09b3ffebe19
August2111 <Info@FlapsOnline.de>
2025-03-18 22:28:31 +0100

    build/targets.mk: use Android NDK r27c, latest LTS Version"

--- build/targets.mk
+++ build/targets.mk
@@ -364,7 +364,7 @@ ifeq ($(TARGET),UNIX)
 endif
 
 ifeq ($(TARGET),ANDROID)
-  ANDROID_NDK ?= $(HOME)/opt/android-ndk-r26d
+  ANDROID_NDK ?= $(HOME)/opt/android-ndk-r27c
 
   ANDROID_SDK_PLATFORM = android-33
   ANDROID_NDK_API = 21
