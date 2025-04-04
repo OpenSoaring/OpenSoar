4c458c53011e1c27a5909424e980f60af72c048c
August2111 <Info@FlapsOnline.de>
2025-04-04 15:38:53 +0200

    [build] add dependency of ProgramVersion to resources

--- build/resource.mk
+++ build/resource.mk
@@ -86,7 +86,7 @@ SVG_TITLE = Data/graphics/title.svg
 # Data/graphics/title_red.svg
 SVG_TMP_TITLE = $(DATA)/temp/graphics/title.svg $(DATA)/temp/graphics/title_red.svg
 # convert to title
-$(DATA)/temp/graphics/%.svg: $(SVG_TITLE) $(topdir)/OpenSoar.config
+$(DATA)/temp/graphics/%.svg: $(SVG_TITLE) $(topdir)/OpenSoar.config $(OUT)/include/ProgramVersion.h
 	# @$(NQ)echo "  TMP_SVG:   $< == $@"
 	@echo  "  TMP_SVG:   $< == $@"
 	$(Q)$(MKDIR) -p $(DATA)/temp/graphics
