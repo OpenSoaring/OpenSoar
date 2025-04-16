bd2b4044fe0f9942b5b6916b47704e72c949b02b
August2111 <Info@FlapsOnline.de>
2025-04-16 09:10:26 +0200

    [SkySight, build] control the debug outputs from make file side

--- build/features.mk
+++ build/features.mk
@@ -7,26 +7,38 @@ ifeq ($(TARGET_IS_KOBO)$(TARGET_IS_DARWIN),nn)
     HAVE_SKYSIGHT := y
 
     ifneq ($(TARGET_IS_OPENVARIO),y)
-        SKYSIGHT_FORECAST := y
         HAVE_GEOTIFF = y
     endif
-
     
-    # for cpp sources:
-    TARGET_CPPFLAGS += -DHAVE_SKYSIGHT
-    TARGET_CPPFLAGS += -DSKYSIGHT_LIVE
+    ifeq ($(HAVE_SKYSIGHT),y)
+        ifneq ($(TARGET_IS_OPENVARIO),y)
+            SKYSIGHT_FORECAST := y
+        endif
+    
+        # for cpp sources:
+        TARGET_CPPFLAGS += -DHAVE_SKYSIGHT
+        TARGET_CPPFLAGS += -DSKYSIGHT_LIVE
 
-    ifeq ($(SKYSIGHT_FORECAST),y)
-        TARGET_CPPFLAGS += -DSKYSIGHT_FORECAST
-    endif
+        ifeq ($(SKYSIGHT_FORECAST),y)
+            TARGET_CPPFLAGS += -DSKYSIGHT_FORECAST
+            TARGET_CPPFLAGS += -DSKYSIGHT_OFFLINE_MODE
+        endif
+
+        # debug feature for SkySight:
+        # TARGET_CPPFLAGS += -DSKYSIGHT_FILE_DEBUG
+        # TARGET_CPPFLAGS += -DSKYSIGHT_REQUEST_LOG
+        # TARGET_CPPFLAGS += -DSKYSIGHT_HTTP_LOG
+
+    endif  # HAVE_SKYSIGHT
+
 else 
   # Kobo, MacOS, iOS,...:
   HAVE_SKYSIGHT := n
   HAVE_GEOTIFF := n
 endif
 
+# check if blanks beside 'n' or 'y':
+# $(info HAVE_GEOTIFF = '$(HAVE_GEOTIFF)')
 ifeq ($(HAVE_GEOTIFF),y)
    TARGET_CPPFLAGS += -DUSE_GEOTIFF
 endif
-
-$(info HAVE_GEOTIFF = '$(HAVE_GEOTIFF)')
