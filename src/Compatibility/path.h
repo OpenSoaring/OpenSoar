03a934388639256e8d715cb635b8804771219e42
August2111 <Info@FlapsOnline.de>
2025-05-18 23:11:58 +0200

    use in Windows the same (valid!) path separator like all others

--- src/Compatibility/path.h
+++ src/Compatibility/path.h
@@ -3,24 +3,16 @@
 
 #pragma once
 
-#ifdef _WIN32
-
-#define DIR_SEPARATOR '\\'
-#define DIR_SEPARATOR_S "\\"
-
-#else /* !_WIN32 */
-
+// use in Windows the same (valid!) path separator like all others:
 #define DIR_SEPARATOR '/'
 #define DIR_SEPARATOR_S "/"
-
-#endif /* !_WIN32 */
 
 static inline bool
 IsDirSeparator(char ch)
 {
 #ifdef _WIN32
   // at Windows both separators are possible!!!
-  return ch == DIR_SEPARATOR || ch == '/';
+  return ch == DIR_SEPARATOR || ch == '\\';
 #else
   return ch == DIR_SEPARATOR;
 #endif
