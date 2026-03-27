92638da121427bb6ba1b240c5d23ec9a046da194
August2111 <Info@FlapsOnline.de>
2026-03-27 22:06:41 +0100

    [utf8] io/FileDescriptor: remove Open with WChar path (Windows only)

--- src/io/FileDescriptor.hxx
+++ src/io/FileDescriptor.hxx
@@ -112,11 +112,6 @@ public:
 	[[nodiscard]]
 	bool Open(const char *pathname, int flags, mode_t mode=0666) noexcept;
 
-#ifdef _WIN32
-	[[nodiscard]]
-	bool Open(const wchar_t *pathname, int flags, mode_t mode=0666) noexcept;
-#endif
-
 	[[nodiscard]]
 	bool OpenReadOnly(const char *pathname) noexcept;
 
