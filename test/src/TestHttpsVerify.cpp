e6f1e588a26de5e0f81bede98212ece43c76e5cd
August2111 <Info@FlapsOnline.de>
2026-04-15 11:45:05 +0200

    [test] TestHttpsVerify redesign this test with an explanation of the wrong behavior, add 2 test cases w/ wron url

--- test/src/TestHttpsVerify.cpp
+++ test/src/TestHttpsVerify.cpp
@@ -8,6 +8,14 @@
 #include "lib/curl/Setup.hxx"
 #include "net/http/Init.hpp"
 
+// TODO(August2111): this test is wrong, because Curl::CoRequest don't make an
+// exception. The windows test sseemsto be ok, because FetchOk returned with 
+// false, but on Linux the return value is true in the error case - and this 
+// shows a wrong test result!
+// The exception handler of Curl::CoRequest is not called, because the error 
+// is deferred and the productive code is not prepared for this. Then the test 
+// could be enabled again.
+
 struct Instance : CoInstance {
   const Net::ScopeInit net_init{GetEventLoop()};
 };
@@ -40,14 +48,27 @@ main()
   plan_skip_all("TLS verification disabled on this platform");
   return exit_status();
 #else
-  plan_tests(2);
+  plan_tests(4);
 
   Instance instance;
 
   ok1(instance.Run(FetchOk(*Net::curl,
-                           "https://download.xcsoar.org/repository")));
-  ok1(!instance.Run(FetchOk(*Net::curl, "https://wrong.host.badssl.com/")));
+    "https://download.xcsoar.org/repository")));
+  // http instead of https should not work:
+  ok1(!instance.Run(FetchOk(*Net::curl,
+    "http://download.xcsoar.org/repository")));
+  // a wrong subpage should not work:too
+  ok1(!instance.Run(FetchOk(*Net::curl,
+    "https://download.xcsoar.org/unknown")));
+  // a complete wrong url should also not work:
+   ok1(!instance.Run(FetchOk(*Net::curl,
+    "https://wrong.host.badssl.com/")));
 
+} catch (...) {
+#if 1 // !!!!!!!!!!!!!!!!!!
+  std::cerr << "Exception !!!!! " << std::endl;
+#endif
+}
   return exit_status();
 #endif
 }
