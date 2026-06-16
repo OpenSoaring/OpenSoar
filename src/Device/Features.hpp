69e3cc0cf92420a4e7efa84670e0854eac2b7d0a
August2111 <Info@FlapsOnline.de>
2026-06-16 14:31:00 +0200

    [Serial] - use an additional driver link (7 instead of 6) for automatic detect
    this 7th port is visible in the moment

--- src/Device/Features.hpp
+++ src/Device/Features.hpp
@@ -3,7 +3,10 @@
 
 #pragma once
 
-static constexpr unsigned NUMDEV = 6;
+
+static constexpr unsigned VISIBLE_NUMDEV = 6;
+static constexpr unsigned REMOTE_PORT = VISIBLE_NUMDEV;  // the last one
+static constexpr unsigned NUMDEV = VISIBLE_NUMDEV+1;
 
 #if defined(ANDROID) || defined(__APPLE__)
 #define HAVE_INTERNAL_GPS
