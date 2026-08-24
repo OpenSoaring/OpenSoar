00e1310c12ad6b59ff3e5c80aaab16ef05f9cdf5
Uwe Augustin <Info@FlapsOnline.de>
2026-08-24 15:38:39 +0000

    src: MSVC and GDI/GLES portability fixes
    Compiler portability (from_chars/array iterators as pointers,
    IntrusiveHashSet NTTP options, <stdexcept>/<iterator> includes,
    MSVC weak-symbol emulation via /alternatename, windows.h macro
    hygiene for DrawText/ERROR/DT_*), POSIX-free file I/O with UTF-8
    paths (_wopen + O_BINARY), jasper/shapelib/zzip MSVC configs and
    resource-loader diagnostics.

--- src/ui/canvas/Icon.cpp
+++ src/ui/canvas/Icon.cpp
@@ -2,6 +2,7 @@
 // Copyright The XCSoar Project
 
 #include "Icon.hpp"
+#include "LogFile.hpp"
 #include "Canvas.hpp"
 #include "Screen/Layout.hpp"
 
@@ -133,6 +134,17 @@ MaskedIcon::LoadResource(ResourceId id, ResourceId mdpi_id,
   const unsigned stretch =
     IconStretchFixed10(source_dpi * ICON_SUPERSAMPLE);
   bitmap.Load(id);
+
+#ifdef USE_WIN32_RESOURCES
+  if (!bitmap.IsDefined())
+    /* make the failing resource identifiable before the assert fires -
+       a typical cause is a corrupt .bmp embedded by the resource
+       compiler after an interrupted graphics conversion */
+    LogFormat("MaskedIcon::LoadResource failed: id=%u big_id=%u "
+              "ultra_id=%u vdpi=%u",
+              (unsigned)id, (unsigned)big_id, (unsigned)ultra_id,
+              Layout::vdpi);
+#endif
 
   assert(IsDefined());
 
